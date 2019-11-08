18e6736e85d23b2e461280e707558a39de6c6ed1
demian44 <demiandes@gmail.com>
2019-11-08 10:48:09 -0300

    Subo API

---
+++ CLASE_2_WebApi/Clase2_API/Clase2_API/Models/Amigo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase2_API.Models
+{
+    public class Amigo
+    {
+        public int ID { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+    }
+}
