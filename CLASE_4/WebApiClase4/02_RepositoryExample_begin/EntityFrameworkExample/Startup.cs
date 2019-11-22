bab2b6441f68f858f82db3fbb5dcfa920bd68d91
demian44 <demiandes@gmail.com>
2019-11-22 11:49:09 -0300

    Clase 4 - Repositories

---
+++ CLASE_4/WebApiClase4/02_RepositoryExample_begin/EntityFrameworkExample/Startup.cs
@@ -0,0 +1,43 @@
+﻿using EntityFrameworkExample.Data;
+using EntityFrameworkExample.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EntityFrameworkExample
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<PersonContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IRepository, MyRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, PersonContext personContext)
+        {
+            personContext.Database.EnsureDeleted();
+            personContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "defaultRoute",
+                    template: "{controller=Person}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
