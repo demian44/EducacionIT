9adb9f0343d9ff09bcf67143e81072e23f347034
demian44 <demiandes@gmail.com>
2019-11-29 12:39:23 -0300

    ultimos cambios

---
+++ CLASE_5/20486D-DevelopingASPNETMVCWebApplications/Allfiles/Mod11/Labfiles/01_Library_begin/Library/Controllers/UserLoginsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Library.Data;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLoginsController : ControllerBase
+    {
+        private readonly LibraryContext _context;
+
+        public UserLoginsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserLogins
+        [HttpGet]
+        public IEnumerable<UserLogin> GetUserLogin()
+        {
+            return _context.UserLogin;
+        }
+
+        // GET: api/UserLogins/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserLogin([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userLogin = await _context.UserLogin.FindAsync(id);
+
+            if (userLogin == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userLogin);
+        }
+
+        // PUT: api/UserLogins/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserLogin([FromRoute] int id, [FromBody] UserLogin userLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userLogin.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userLogin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserLoginExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserLogins
+        [HttpPost]
+        public async Task<IActionResult> PostUserLogin([FromBody] UserLogin userLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.UserLogin.Add(userLogin);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserLogin", new { id = userLogin.ID }, userLogin);
+        }
+
+        // DELETE: api/UserLogins/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserLogin([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userLogin = await _context.UserLogin.FindAsync(id);
+            if (userLogin == null)
+            {
+                return NotFound();
+            }
+
+            _context.UserLogin.Remove(userLogin);
+            await _context.SaveChangesAsync();
+
+            return Ok(userLogin);
+        }
+
+        private bool UserLoginExists(int id)
+        {
+            return _context.UserLogin.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ CLASE_5/20486D-DevelopingASPNETMVCWebApplications/Allfiles/Mod11/Labfiles/01_Library_begin/Library/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class UserLogin
+    {
+        public int ID { get; set; }
+
+        public string User { get; set; }
+
+        public string Password { get; set; }
+    }
+}
