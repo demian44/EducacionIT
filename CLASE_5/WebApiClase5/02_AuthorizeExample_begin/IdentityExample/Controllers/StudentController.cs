96196b4d9c6208bc955e6b0a624160e0aa70939f
demian44 <demiandes@gmail.com>
2019-11-29 12:01:25 -0300

    Seguridad con autenticacion y manejo de roles

--- CLASE_5/WebApiClase5/02_AuthorizeExample_begin/IdentityExample/Controllers/StudentController.cs
+++ CLASE_5/WebApiClase5/02_AuthorizeExample_begin/IdentityExample/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using IdentityExample.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -18,7 +19,7 @@ namespace IdentityExample.Controllers
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
             return View();
         }
