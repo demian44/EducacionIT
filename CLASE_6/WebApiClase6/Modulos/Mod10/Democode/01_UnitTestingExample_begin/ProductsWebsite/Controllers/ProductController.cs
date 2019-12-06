bfe5e67a0219dc3ab7d951a4efa0cf62efa457b0
demian44 <demiandes@gmail.com>
2019-12-06 11:47:21 -0300

    agrego modificaciones

--- CLASE_6/WebApiClase6/Modulos/Mod10/Democode/01_UnitTestingExample_begin/ProductsWebsite/Controllers/ProductController.cs
+++ CLASE_6/WebApiClase6/Modulos/Mod10/Democode/01_UnitTestingExample_begin/ProductsWebsite/Controllers/ProductController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using ProductsWebsite.Repositories;
+using System.Linq;
 
 namespace ProductsWebsite.Controllers
 {
@@ -25,7 +22,7 @@ namespace ProductsWebsite.Controllers
         public IActionResult GetProduct(int id)
         {
             var products = _repository.GetProducts();
-            var product = products.Where(p => p.Id != id).FirstOrDefault();
+            var product = products.Where(p => p.Id == id).FirstOrDefault();
             return View(product);
         }
 
