e9f181c9d5b5ef60df43ed4ff09c132f552dcb62
demian44 <demiandes@gmail.com>
2019-12-13 11:38:50 -0300

    Clase 7 - Web Socket, Cache y Session

---
+++ WebApiClase7/Mod12/Democode/03_SignalRExample_begin/SignalRExample/Hubs/SquaresHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRExample.Services;
+using System.Threading.Tasks;
+
+namespace SignalRExample.Hubs
+{
+    public class SquaresHub : Hub
+    {
+        private ISquareManager _manager;
+        public SquaresHub(ISquareManager manager)
+        { _manager = manager; }
+
+        public async Task SwapColor(int rowIndex, int columnIndex)
+        {
+            _manager.SwapColor(rowIndex, columnIndex);
+            await Clients.Others.SendAsync("SwapSquareColor", rowIndex, columnIndex);
+        }
+    }
+}
---
+++ WebApiClase7/Mod12/Democode/03_SignalRExample_begin/SignalRExample/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRExample.Middleware;
+using SignalRExample.Services;
+using SignalRExample.Services;
+using SignalRExample.Hubs;
+
+namespace SignalRExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+            services.AddMvc();
+            services.AddSingleton<ISquareManager, SquareManager>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<SquaresHub>("/squareshub");
+            });
+
+            app.UseStaticFiles();
+
+            app.UseNodeModules(env.ContentRootPath);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}",
+                defaults: new { controller = "Square", action = "Index" });
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
